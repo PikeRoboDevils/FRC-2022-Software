07e49999212bceaa4ad957ddba4a0a65b78f6079
Ryan Blue <ryanzblue@gmail.com>
2022-01-17 19:31:43 -0500

    Add drivetrain constants

---
+++ src/main/java/org/pikerobodevils/frc2022/Constants.java
@@ -0,0 +1,15 @@
+package org.pikerobodevils.frc2022;
+
+public class Constants {
+    public class DrivetrainConstants {
+        public static final int LEFT_LEADER_ID = 1;
+        public static final int LEFT_FOLLOWER_ONE_ID = 3;
+        public static final int LEFT_FOLLOWER_TWO_ID = 5;
+
+        public static final int RIGHT_LEADER_ID = 2;
+        public static final int RIGHT_FOLLOWER_ONE_ID = 4;
+        public static final int RIGHT_FOLLOWER_TWO_ID = 6;
+    }
+
+
+}
